b6be4fa95165048b7edebbad24bcd96b738e1620
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-18 21:21:41 -0800

    libqdutils: Fix for VNDK_VERSION=current
    Change-Id: I630d3fc69a066c9c391f909fc6cf96190b2cc988

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -36,7 +36,7 @@ include $(CLEAR_VARS)
 
 LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
 LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
+LOCAL_SHARED_LIBRARIES          := $(common_libs)
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
